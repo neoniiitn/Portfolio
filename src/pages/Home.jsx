5e8069a9a1d5d886167492179c7eb639460286a2
sparsh912 <sparshmaheshwari0908@gmail.com>
2025-11-02 16:54:20 +0530

    adding theme , css , dark-light mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import { Navbar } from "../components/Navbar";
+import { ThemeToggle } from "../components/ThemeToggle";
+// import { StarBackground } from "@/components/StarBackground";
+// import { HeroSection } from "../components/HeroSection";
 import React from 'react'
 
-function Home() {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+export default function Home(){
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      {/* <StarBackground /> */}
+
+      {/* Navbar */}
+      {/* <Navbar /> */}
+      {/* Main Content */}
+      <main>
+        {/* <HeroSection /> */}
+      </main>
+
+    </div>
+  );
+};
